63afadd86275d6e239fba4a8902836f8d50a3fe2
Eddy Mio <contact@remotehireman.fr>
2018-04-11 18:26:27 +0200

    Enabling the bundle from within the bundles.php file

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+
+    App\RHM\PlatformBundle\RHMPlatformBundle::class => ['all' => true],
+
 ];
