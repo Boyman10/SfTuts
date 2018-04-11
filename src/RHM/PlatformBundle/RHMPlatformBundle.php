c76c791ec0f04a7a245b40e438ad8853d89a2f08
Eddy Mio <contact@remotehireman.fr>
2018-04-11 18:37:58 +0200

    Mimic the demo bundle : https://github.com/KnpLabs/symfony-with-composer/tree/master/src/Acme/DemoBundle

--- src/RHM/PlatformBundle/RHMPlatformBundle.php
+++ src/RHM/PlatformBundle/RHMPlatformBundle.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * User: bob
  * Date: 11/04/18
  * Time: 18:21
+ * @link https://symfony.com/doc/current/bundles.html
  */
 class RHMPlatformBundle extends Bundle
 {
