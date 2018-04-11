5db10e4cf508293cb195f5f7fdd353d33b075166
Eddy Mio <contact@remotehireman.fr>
2018-04-11 18:24:53 +0200

    Adding new bundle by creating a new class

---
+++ src/RHM/PlatformBundle/RHMPlatformBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\RHM\PlatformBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by RHM.
+ * New platform bundle
+ * User: bob
+ * Date: 11/04/18
+ * Time: 18:21
+ */
+class RHMPlatformBundle extends Bundle
+{
+
+}
