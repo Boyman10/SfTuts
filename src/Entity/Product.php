5f5636114a2bda39815ed2180c7be42cb3fb13d0
Boyman10 <contact@yasi.fr>
2018-04-17 21:54:08 +0200

    Created entity class for testing purpose

---
+++ src/Entity/Product.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+/**
+ * Created by bill.
+ * Entity class for testing purpose
+ * User: John
+ * Date: 4/17/2018
+ * Time: 9:52 PM
+ */
+class Product
+{
+    private $id;
+    private $name;
+    private $description;
+
+    /**
+     * Product constructor.
+     * @param $id
+     * @param $name
+     * @param $description
+     */
+    public function __construct($id, $name, $description)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
